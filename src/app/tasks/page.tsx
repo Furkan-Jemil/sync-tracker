67e2f2021785803eebcaca3111225dbb1d2c9f6b
ibnu_jemil <furkanjemil54@gmail.com>
2026-02-27 09:40:05 +0300

    feat(tasks): add tasks page

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+import { DashboardShell } from "@/components/dashboard/DashboardShell";
+
+export default function TasksPage() {
+  return <DashboardShell initialTab="tasks" />;
+}
+
