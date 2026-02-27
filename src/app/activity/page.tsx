3f1949f8dfa47634454a7c9ecb2cf56402755ee6
ibnu_jemil <furkanjemil54@gmail.com>
2026-02-27 09:40:11 +0300

    feat(activity): add activity page

---
+++ src/app/activity/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+import { DashboardShell } from "@/components/dashboard/DashboardShell";
+
+export default function ActivityPage() {
+  return <DashboardShell initialTab="logs" />;
+}
+
